a27df3efce459a3a01df8dd9a95aede7f59d1d41
Densi Jashari <107101355+denisjashari54@users.noreply.github.com>
2023-12-03 21:54:24 +0100

    Create Spieler.cs

---
+++ Projekt/Spieler.cs
@@ -0,0 +1,11 @@
+class Spieler
+{
+    public string Name { get; set; }
+    public char Symbol { get; set; }
+
+    public Spieler(string name, char symbol)
+    {
+        Name = name;
+        Symbol = symbol;
+    }
+}
